e11c558c389a0cc365b0f80f42b4bfec36454262
crccheck <c@crccheck.com>
2012-07-11 19:58:46 -0500

    annual checkup and refactor
    changes from re-visiting after a year:
    remove unnecessary jquery calls, cache others
    
    only do DOM manipulation once, subsequent calls to tablebars() don't
    tear down and rebuild, they re-use.
    
    API CHANGE: look for a data attribute instead of a class. This will
    certainly change soon.
    
    Avoid potential mangling: Don't do .html(.text()), just do .html(.html())

--- jquery.tablebars.js
+++ jquery.tablebars.js
@@ -1,50 +1,55 @@
 // jquery tablebars plugin
-// version 0.1
+// version 0.2a
 // usage: $('table').tablebars()
-// to turn on bars for a column, add the 'tablebars' class to the corresponding
-// header. By default, the bars have no style. you'll can style them with:
+// to turn on bars for a column, add the 'data-tablebars=1' class to the corresponding
+// header. By default, the bars have no style. An example css declartion is:
 //
 //      .ui-tablebar { color: #333; background: #DACD99; }
 //
 // limitations: table must have a thead, data must be simple
 jQuery.fn.tablebars = function(){
-    $(this).each(function(){
-        var table = $(this);
+  this.each(function(i, table){
+    var $table = $(table);
 
-        function pickColumns(table){
-            var columns = [];
-            table.find('thead > tr:eq(0)').children().each(function(i, cell){
-                if ($(cell).hasClass('tablebars')) columns.push(i);
-            });
-            return columns;
+    // decide which columns get tablebars
+    function pickColumns($table){
+      var columns = [];
+      $table.find('thead > tr:eq(0)').children().each(function(i, cell){
+        if ($(cell).attr('data-tablebars')) columns.push(i);
+      });
+      return columns;
+    }
+
+    function processColumn(set){
+      var data = [],
+          rawdata = [],
+          max = 0,
+          width = set.eq(0).width();
+      set.each(function(i, cell){
+        var rawvalue = rawdata[i] = $(cell).html();
+        var value = data[i] = Math.max(0, parseFloat(rawvalue.replace(/[^0-9.\-]+/g, '')));
+        max = Math.max(max, value);
+      });
+      if (!max) max = 1;
+      set.each(function(i, cell){
+        var $cell = $(cell),
+            $bar = $cell.find('span.ui-tablebar');
+        if (!$bar.length) {
+          $bar = $('<span class="ui-tablebar" style="overflow:hidden; position:absolute; width:0;"></span>');
+          $cell.css('position', 'relative').prepend($bar);
         }
+        $bar.width(width * data[i] / max).html(rawdata[i]);
+      });
+    }
 
-        function processColumn(set){
-            set.children('span.ui-tablebar').remove();
-            var data = [],
-                rawdata = [],
-                max = 0,
-                width = set.eq(0).width();
-            set.each(function(i, cell){
-                var rawvalue = rawdata[i] = $(cell).text();
-                var value = data[i] = Math.max(0, +rawvalue.replace(/[^0-9.\-]+/g,''));
-                max = Math.max(max, value);
-            });
-            if (!max) max = 1;
-            set.css('position', 'relative').prepend($('<span class="ui-tablebar" style="overflow:hidden; position:absolute; width:0;"></span>'));
-            set.each(function(i, cell){
-                $(cell).find('span.ui-tablebar').width(width * data[i] / max).html(rawdata[i]);
-            });
-        }
+    function collectCells(columns){
+      columns.forEach(function(index){
+        var set = $table.find('tbody > tr > td:nth-child(' + (index + 1) + ')');
+        processColumn(set);
+      });
+    }
 
-        function collectCells(columns){
-            columns.forEach(function(index){
-                var set = $(table.find('tbody > tr > td:nth-child(' + (index + 1) + ')'));
-                processColumn(set);
-            });
-        }
-
-        collectCells(pickColumns(table));
-    });
-    return this;
+    collectCells(pickColumns($table));
+  });
+  return this;
 };
